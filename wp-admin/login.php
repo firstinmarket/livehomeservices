f886dd3c3175a134c50cf9ddb66b9f600ee7d27a
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-09-05 08:17:58 +0530

    update viewport

--- wp-admin/login.php
+++ wp-admin/login.php
@@ -24,6 +24,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
 	<meta charset="UTF-8">
 	<title>Login - Live Home Services Admin</title>
+	  <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body class="bg-gray-100 flex items-center justify-center h-screen">
